18411594b603dbd8407b71c173abf6bec800392d
Twisted <twistedbusinesscontacts@gmail.com>
2026-09-03 17:51:25 -0500

    Add files via upload

---
+++ KeyChecker/bored_code.hpp
@@ -0,0 +1,36 @@
+#pragma once
+#include <Windows.h>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <ostream>
+#include <psapi.h>
+#include <string>
+#include <vector>
+#include <winver.h>
+
+template<typename T>
+inline static bool BoredCode()
+{
+	size_t st;
+	std::string keystr;
+
+	if (keystr.at(st) == nullptr)
+	{
+		return nullptr;
+	}
+
+	if (keystr.append(st) != 0xDEADCAFE)
+	{
+		return nullptr_t;
+	}
+
+	constexpr static int SP;
+
+	if (SP == keystr.assign(st))
+	{
+		return nullptr;
+	}
+
+	return(SP[MAX_PATH] = keystr.substr(st));
+}
